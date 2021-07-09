bb4f16b72c4ab496a4c84abda0f77f9f29ad33f5
luisangelquinones <luisangelq3673@gmail.com>
2021-07-09 10:25:51 -0500

    finish prototypes

--- 22-Prototypes/js/04-app.js
+++ 22-Prototypes/js/04-app.js
@@ -1,1 +1,16 @@
 console.log("*******Inherit prototype********");
+
+function Person(name, balance, phone) {
+  //we can inherit from another object 
+  Client3.call(this, name, balance)
+  this.phone = phone;
+}
+Person.prototype = Object.create( Company3.prototype )
+Person.prototype.constructor = Company3;
+
+Person.prototype.personPhone = function() {
+    return this.phone
+}
+
+const karina = new Person("Karina", 400500, 3326264356);
+console.log(`${karina.companyLoyal()} and the phone is ${karina.personPhone()}`);
