35d9d04ccf477e6dbeeeb31e4d34890bbcbeb348
luisangelquinones <luisangelq3673@gmail.com>
2021-07-16 10:29:13 -0500

    expense project

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (budgetValue === "" || budgetValue <= 0 || isNaN(budgetValue)) {
         window.location.reload();
       } else {
-        budget = new Budget(budgetValue);
+        budget = new Budget(budgetValue)
         ui.insertBudget(budget);
       }
     }
@@ -43,34 +43,75 @@ class Budget {
     this.remain = budget;
     this.expenses = [];
   }
+
+  newExpense(expense) {
+    this.expenses = [...this.expenses, expense]
+  }
 }
+
 class UI {
   insertBudget(amount) {
     const { budget, remain } = amount;
     document.querySelector("#total").textContent = budget;
     document.querySelector("#restante").textContent = remain;
   }
+
   printAlert(message, type) {
+    const removeError = document.querySelector(".removeError");
+    if (removeError) {
+      removeError.remove();
+    }
     const divMessage = document.createElement("div");
-    divMessage.classList.add("text-center", "alert");
+    divMessage.classList.add("text-center", "alert", "removeError");
 
     if (type === "error") {
-        divMessage.classList.add("alert-danger")
+      divMessage.classList.add("alert-danger");
+    } else {
+      divMessage.classList.add("alert-success");
     }
+    divMessage.textContent = message;
+
+    document.querySelector(".primario").insertBefore(divMessage, form);
+
+    setTimeout(() => {
+      divMessage.remove();
+    }, 2000);
+  }
+
+  addExpenseList(expenses) {
+    console.log(expenses);
   }
 }
 
 //Instance
 const ui = new UI();
+let budget;
+
 
 //Functions
 const addExpense = (e) => {
   e.preventDefault();
-   
+
   const item = document.querySelector("#gasto").value;
   const amount = document.querySelector("#cantidad").value;
 
   if (item === "" || amount === "") {
-      ui.printAlert("Both Fields Are Required", "error")
+    ui.printAlert("Both Fields Are Required", "error");
+  } else if ( amount <= 0 || isNaN(amount)){
+    ui.printAlert("Amount No Valid", "error");
   }
+
+  const expense = {item, amount, id: Date.now()}
+
+  //Add new expense
+  budget.newExpense(expense)
+  
+  ui.printAlert("Expense Added Successfully");
+
+  //Print Expenses
+  const { expenses } = budget;
+
+  ui.addExpenseList(expenses);
+
+  form.reset()
 };
