86630460c58de71e87103adad0b8cc0e73204dcd
Luis Angel Quiñones Guerrero <luis.quinones@compac.com.mx>
2021-07-23 19:24:01 -0500

    Symbols

---
+++ 27-SetsMapsSymbols/js/05-app.js
@@ -0,0 +1,26 @@
+console.log("*********** Symbols ************");
+
+//Allows you to create a unique property 
+//there cannot be 2 alike
+
+const sym = Symbol();
+const sym2 = Symbol();
+
+if (sym === sym2) {
+    console.log("Symbols are the same");
+    
+} else {
+    console.log("Symbols are different");
+}
+
+const name = Symbol();
+const lastName = Symbol();
+
+const person1 = {}
+
+person1[name] = "John";
+person1[lastName] = "Doe";
+person1.clientType = "Premium";
+person1.age = 30;
+
+console.log(person1);
