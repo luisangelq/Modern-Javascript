8008ac0bda9abbb954db6136b98903aea61e0223
Luis Angel Quiñones Guerrero <luis.quinones@compac.com.mx>
2021-08-06 18:32:31 -0500

    Pixabay continue

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -1,6 +1,10 @@
 const result = document.querySelector("#result");
 const form = document.querySelector("#form");
 
+const recordsPerPage = 30;
+let totalPages;
+let pages;
+
 window.onload = () => {
   form.addEventListener("submit", validateForm);
 };
@@ -48,12 +52,26 @@ function showAlert(message) {
 
 function searchImages(topic) {
   const key = "22805124-d050eb8070bbfa21bdc6846ec";
-  const url = `https://pixabay.com/api/?key=${key}&q=${topic}`;
+  const url = `https://pixabay.com/api/?key=${key}&q=${topic}&per_page=${recordsPerPage}`;
 
   fetch(url)
     .then((response) => response.json())
-    .then((json) => showImages(json.hits))
-    .catch((error) => onsole.log(error));
+    .then((json) => {
+      totalPages = calculatePages(json);
+      showImages(json.hits)
+    })
+    .catch((error) => console.log(error));
+}
+
+function calculatePages(images) {
+  return Math.ceil(images.totalHits / recordsPerPage);
+}
+
+//Generator for each page
+function *generatePages(totalPages) {
+  for (let i = 1; i <= totalPages; i++) {
+    yield i
+  }
 }
 
 function showImages(images) {
@@ -65,17 +83,33 @@ function showImages(images) {
     const { previewURL, likes, views, largeImageURL } = image;
 
     result.innerHTML += `
-        <div class="w.1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
-          <div class="bg-white">
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4 "> 
+          <div class="">
             <img class="w-full" src="${previewURL}">
 
-            <div class="p-4">
+            <div class="text-center bg-white p-4">
               <p ><span class="font-bold">${likes}</span> Likes</p>
               <p ><span class="font-bold">${views}</span> Views</p>
+
+              <a class="block w-full bg-blue-600 hover:bg-blue-400 text-white uppercase font-bold rounded" 
+              href="${largeImageURL}" 
+              rel="noopener noreferrer" 
+              target="_blank">
+                Watch Image
+              </a>
             </div>
           </div>
           
         </div>
       `;
   });
+
+  
+  printPages();
+}
+
+function printPages() {
+  pages = generatePages(totalPages);
+  console.log(pages.next().value);
+  console.log(totalPages);
 }
